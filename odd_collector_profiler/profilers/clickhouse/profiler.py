1b4a8da28c8a57d9ab2d4a26e320e656ec2c985a
ykobuk <kobukstr@gmail.com>
2023-04-03 16:19:34 +0300

    Remove obsolete var in the clickhouse profiler.

--- odd_collector_profiler/profilers/clickhouse/profiler.py
+++ odd_collector_profiler/profilers/clickhouse/profiler.py
@@ -8,22 +8,6 @@ from odd_collector_profiler.domain.config import ClickHouseConfig
 from odd_collector_profiler.domain.profiler import Profiler
 from odd_collector_profiler.profilers import DATA_PROFILER
 
-SYS_SCHEMAS = {
-    "db_owner",
-    "db_accessadmin",
-    "db_securityadmin",
-    "db_ddladmin",
-    "db_backupoperator",
-    "db_datareader",
-    "db_datawriter",
-    "db_denydatareader",
-    "db_denydatawriter",
-    "INFORMATION_SCHEMA",
-    "sys",
-    "guest",
-    "system",
-}
-
 
 class ClickHouseProfiler(RDBProfiler):
     config_model = ClickHouseConfig
@@ -32,7 +16,7 @@ class ClickHouseProfiler(RDBProfiler):
 
 def register_profiler(config: dict[str, Any]) -> Profiler:
     config = ClickHouseConfig.parse_obj(config)
-    repository = RDBRepository(config=config, skip_schemas=SYS_SCHEMAS)
+    repository = RDBRepository(config=config)
     generator = ClickHouseGenerator(
         host_settings=config.host, databases=config.database
     )
